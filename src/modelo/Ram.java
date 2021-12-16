e6c0c393fabc5542c1e39a60af6444a57ee5c2b3
vcanor01 <72250673+vcanor01@users.noreply.github.com>
2021-12-16 13:38:30 +0100

    generar ram

--- src/modelo/Ram.java
+++ src/modelo/Ram.java
@@ -1,18 +1,38 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Ram {
 	
 	private int gb;
+	private final int MIN_GB=2;
+	private final int MAX_GB=16;
 
-	public Ram(int gb) {
+	
+	public Ram() {
 		super();
-		this.gb = gb;
+		this.gb = generarRam();
 	}
 
 	public int getGb() {
 		return gb;
+	}
+	
+	public int generarRam(){
+		Random r = new Random();
+		List<Integer> listaRam = r.ints(MIN_GB,MAX_GB+1).limit(30).distinct().boxed().collect(Collectors.toList());
+		listaRam.removeIf((n)-> (n%2 !=0));
+		listaRam.forEach((a)-> System.out.println(a.toString()));
+		
+	
+		System.out.println("ultimo numero:"+listaRam.get(r.nextInt(listaRam.size())));
+		return listaRam.get(r.nextInt(listaRam.size()));
 	}
 
 	@Override
