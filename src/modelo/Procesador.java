e7d355780930cf18a55892b5c4d68f4fc6796d4e
jose <jose@DESKTOP-3I2V74C>
2021-12-16 14:05:54 +0100

    Procesador

--- src/modelo/Procesador.java
+++ src/modelo/Procesador.java
@@ -1,6 +1,9 @@
 package modelo;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Procesador {
 	
@@ -8,18 +11,39 @@ public class Procesador {
 	private int nucleos;
 	private float velocidad;
 	
-	private final float MIN_VELOCIDAD= 1.3f;//1.3 ghz
-	private final float MAX_VELOCIDAD= 3f;//3ghz
-	private final int MIN_NUCLEOS=1;
+	private float minVelocidad= 0.9f;//1.3 ghz
+	private float maxVelocidad= 1.4f;//3ghz
+	private final float INCREMENTO = 0.5f;
+	private final int MIN_NUCLEOS=2;
 	private final int MAX_NUCLEOS=8;
 	
 	public Procesador(String nombre, int nucleos, float velocidad) {
 		super();
 		this.nombre = nombre;
-		this.nucleos = nucleos;
+		this.nucleos = generarNucleos();
 		this.velocidad = velocidad;
 	}
 	
+	public int generarNucleos(){
+		Random r = new Random();
+		List<Integer> listaNucleos = r.ints(MIN_NUCLEOS,MAX_NUCLEOS+1).limit(30).distinct().boxed().collect(Collectors.toList());
+		listaNucleos.removeIf((n)-> (n%2 !=0));
+	
+		return listaNucleos.get(r.nextInt(listaNucleos.size()));
+	}
+	
+	public float generarVelocidad() {
+		int nucleos = (this.nucleos - MIN_NUCLEOS)/2;
+		float suma = nucleos * this.INCREMENTO;
+		minVelocidad += suma;
+		maxVelocidad += suma;
+		return getGhzRandom(maxVelocidad, minVelocidad);
+	}
+	
+	public int getGhzRandom(int max, int min) {
+		int mwh = (int) (Math.random()*(max-min)+min);
+		return mwh;
+	}
 	
 	public String getNombre() {
 		return nombre;
